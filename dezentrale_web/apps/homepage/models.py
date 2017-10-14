cbe826b2081292e16b4dc4cf34e4844437b14abb
hackopedia31 <wladi.schneider13@googlemail.com>
2017-10-14 15:15:15 +0200

    finish simple homepage with twitter widget

--- dezentrale_web/apps/homepage/models.py
+++ dezentrale_web/apps/homepage/models.py
@@ -1,20 +1,12 @@
 from django.db import models
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
-from wagtail.wagtailcore.fields import RichTextField
-
 
 # TODO: make get_children that way, that it supports the blogs
 class HomePage(Page):
-
-    def get_blog_intro(Page):
-        # display 3 new blog entrys
-        pass
-
     heading = models.CharField()
     subheading = models.CharField()
     intro = models.TextField()
-
     content_panels = Page.content_panels + [
         FieldPanel('heading'),
         FieldPanel('subheading'),
